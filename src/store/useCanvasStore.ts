47c97b82d16b2e5814c377567ea925165c6944d3
Pranav Rapelli <rapellipranav1@gmail.com>
2026-01-03 18:56:01 -0500

    brain

---
+++ src/store/useCanvasStore.ts
@@ -0,0 +1,49 @@
+import { create } from 'zustand';
+import {
+  Node,
+  Edge,
+  NodeChange,
+  EdgeChange,
+  Connection,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+} from 'reactflow';
+
+interface CanvasStore {
+  nodes: Node[];
+  edges: Edge[];
+  onNodesChange: (changes: NodeChange[]) => void;
+  onEdgesChange: (changes: EdgeChange[]) => void;
+  onConnect: (connection: Connection) => void;
+  addNode: (node: Node) => void;
+}
+
+export const useCanvasStore = create<CanvasStore>((set, get) => ({
+  nodes: [],
+  edges: [],
+
+  onNodesChange: (changes: NodeChange[]) => {
+    set({
+      nodes: applyNodeChanges(changes, get().nodes),
+    });
+  },
+
+  onEdgesChange: (changes: EdgeChange[]) => {
+    set({
+      edges: applyEdgeChanges(changes, get().edges),
+    });
+  },
+
+  onConnect: (connection: Connection) => {
+    set({
+      edges: addEdge(connection, get().edges),
+    });
+  },
+
+  addNode: (node: Node) => {
+    set({
+      nodes: [...get().nodes, node],
+    });
+  },
+}));
